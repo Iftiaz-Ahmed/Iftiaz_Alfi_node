5f8d83889a95c848696885918885ca33cc42eec4
iftiaz <iftiaz.ahmed@yahoo.com>
2023-11-18 03:28:22 -0500

    port issue fix

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
-const http = require('http') ; 
+const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-http.createServer((req, res) =>{
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
     console.log(req.url);
-    if(req.url ==='/'){ // home page
-
-       fs.readFile(path.join(__dirname,'public','index.html'),(err,content)=>{
-
-        if (err) throw err ;
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.end(content)
-
-       })
-
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        });
+    } else if (req.url === '/api') {
+        fs.readFile(path.join(__dirname, 'public', 'db.json'), (err, content) => {
+            if (err) throw err;
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(content);
+        });
+    } else {
+        res.end("<h1>404 Nothing is here</h1>");
     }
-    else if (req.url ==='/api'){
-        fs.readFile(path.join(__dirname,'public','db.json'),(err,content)=>{
-
-            if (err) throw err ;
-            res.writeHead(200, {'Content-Type': 'application/json'})
-            res.end(content)
-    
-           })
-    
-    }
-    else{
-        res.end("<h1> 404 Nothing is here </h1>")
-    }
-
-    
-
-}).listen(3000, ()=>console.log("Server is running"));
-
- 
+}).listen(PORT, () => console.log(`Server is running on port ${PORT}`));
